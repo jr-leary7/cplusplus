cfe7b443c997aa54d737fdacc6708c0379efd7f8
Jack Leary <jrleary@live.unc.edu>
2019-11-10 15:17:28 -0500

    first c++ commit

---
+++ sandbox1/sandbox1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std; 
+
+// global variable declaration
+int a = 17;
+
+// defining constants
+#define SWAG "max";
+#define MILES 100;
+#define LINEBREAK '\n';
+
+int main() {
+    string t = "sayonara";
+    cout << SWAG;
+    cout << LINEBREAK;
+    cout << MILES;
+    cout << '\n';
+}
