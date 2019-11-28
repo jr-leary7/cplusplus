3c77c24af0c6a048ab83ef35bc90a41df08d3297
Jack Leary <jrleary@live.unc.edu>
2019-11-28 16:05:16 -0500

    PCA

---
+++ pca_implementation/pca_implementation.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+#include <cfloat>
+#include <stdlib.h>
+
+// remember, arrays are initialized with i.e. int x[2][3] (2 rows and 3 columns)
+
+int x[10][10];
+
+for i in 0 to last array index; {
+     for j in 0 to last array index; {
+        x[i][j] = rand() % 100;
+    }
+}
+
+print(x)
